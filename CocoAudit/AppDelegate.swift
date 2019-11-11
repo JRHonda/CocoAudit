374fe9f1d4cbc56345655d62f6dc111d9794efe7
Justin Honda <justinohonda@gmail.com>
2019-11-11 17:31:31 -0500

    Initial Commit

---
+++ CocoAudit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoAudit
+//
+//  Created by Justin Honda on 11/11/19.
+//  Copyright © 2019 Justin Honda. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
