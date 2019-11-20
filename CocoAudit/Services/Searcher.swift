dc057e83fd4e2978bc9033570e22508a7963dbb7
Justin Honda <justinohonda@gmail.com>
2019-11-19 22:00:30 -0500

    +Working search implementation

--- CocoAudit/Services/Searcher.swift
+++ CocoAudit/Services/Searcher.swift
@@ -14,3 +14,76 @@ struct Searcher {
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ 
+ What are the attitudes, practices, and processes at
+ organizations that you think most hurt cyber-security?
+ 
+ 1. Time not worth it to spend 2-3 months to test and ensure security.
+ 
+ 2. Organization system architecture puts cyber security front and center. However, software development of many programs made cyber-security 3rd or 4th on the list of concerns.
+ 
+ 3.
+ 
+ */
+
+
+
+
+
+
+/*
+ 
+ You have one day to help improve a developer's understanding of
+ how to improve software security. What would be the ten items that
+ you would put on the list and why?
+ 
+ 1. Understand the types of threats (SQL Injection, XSS, etc.)
+        - This can help the developer understand how to prevent these attacks to code properly
+        - to battle against it.
+ 2. Knowing how to audit software and identify what to look for that could lead to security incidents
+        - During code review, one can catch mistakes that might otherwise make into production.
+        - Dependency updates
+ 3. Knowing resources to use to research CVEs
+        - NIST, MITRE, RedHat
+ 4. Understanding past security incidents
+        - To prevent implementing the causes in future development
+ 5. Staying on top of updates
+        - Updated software typically comes with important security patches.
+ 6. Understanding how to securely implement authorization protocols
+        - Since most applications implement authorization to use their apps and prevent unauthorized access to features and sensitive information
+ 7. Software testing
+        - To test as many things as possible to create software that is hopefully as secure as possible
+ 8.
+ 9.
+ 10.
+ 
+ */
